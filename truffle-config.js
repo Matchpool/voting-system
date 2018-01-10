c28de48346f3ee0e643e97f12623eb713520458f
Michael O'Rourke <orocim@gmail.com>
2018-01-10 16:36:18 -0500

    fixed non compile

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
-var HDWalletProvider = require("./src/node_modules/truffle-hdwallet-provider");
+//var HDWalletProvider = require("./src/node_modules/truffle-hdwallet-provider");
 
-var mnemonic = "YOUR ACCOUNT MENOMIC HERE"
+//var mnemonic = "YOUR ACCOUNT MENOMIC HERE"
 // e.g. "lamp kitchen cotton van whip happy reflect coil reef advance grow adapt
 
-var infurakey = "YOUR INFURA KEY HERE"
+//var infurakey = "YOUR INFURA KEY HERE"
 // Register at infura for a key
 
 module.exports = {
@@ -12,19 +12,20 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infurakey)
-      },
-      network_id: 3,
-      gas: 4612388
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + infurakey)
-      },
-      network_id: 4
-    }
-  }
+     }
+  //,
+  //   ropsten: {
+  //     provider: function() {
+  //       return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infurakey)
+  //     },
+  //     network_id: 3,
+  //     gas: 4612388
+  //   },
+  //   rinkeby: {
+  //     provider: function() {
+  //       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + infurakey)
+  //     },
+  //     network_id: 4
+  //   }
+   }
 };
